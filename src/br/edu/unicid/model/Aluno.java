a0336b43fef145c806751d5fb6950d4f407b1b91
arthur-miranda-da-silva <112426931+arthur-miranda-da-silva@users.noreply.github.com>
2023-05-06 20:29:45 -0300

    Update Aluno.java

--- src/br/edu/unicid/model/Aluno.java
+++ src/br/edu/unicid/model/Aluno.java
@@ -7,23 +7,59 @@ public class Aluno {
 	private String dtaNascimento;
 	private int idade ;
 	private String endereco;
+	private String cpf;
+	private String municipio;
+	private String uf;
+	private String telefone;
 
 	// construtor vazio
 	public Aluno() {
 		// TODO Auto-generated constructor stub
 	}
 	// construtor com campos
-	public Aluno(int rgm, String nome, String email, String dtaNascimento, int idade, String endereco) {
+	public Aluno(int rgm, String nome, String dtaNascimento, String cpf, String endereco, String municipio, String uf, String telefone, String email) {
 		this.rgm = rgm;
 		this.nome = nome;
-		this.email = email;
 		this.dtaNascimento = dtaNascimento;
-		this.idade = idade;
+		this.cpf =cpf;
+		this.municipio = municipio;
+		this.uf = uf;
+		this.endereco =endereco;
+		this.email = email;
 		this.endereco = endereco;
+		this.telefone = telefone;
 	}
 	// getters e setters
+	
+	public Aluno(int ra, String nome2, String email2, String nascimento, int idade2, String endereco2) {
+		// TODO Auto-generated constructor stub
+	}
 	public int getRgm() {
 		return rgm;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getMunicipio() {
+		return municipio;
+	}
+	public void setMunicipio(String municipio) {
+		this.municipio = municipio;
+	}
+	public String getUf() {
+		return uf;
+	}
+	public void setUf(String uf) {
+		this.uf = uf;
 	}
 	public void setRgm(int rgm) {
 		this.rgm = rgm;
